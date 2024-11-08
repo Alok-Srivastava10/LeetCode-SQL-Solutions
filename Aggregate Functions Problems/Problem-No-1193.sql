9e2fbed36c0259372787e05af0638a4059a28e1b
28-12-03 <alok27141@gmail.com>
2024-11-08 22:46:26 +0530

    Aggregate Functions Section Created

---
+++ Aggregate Functions Problems/Problem-No-1193.sql
@@ -0,0 +1,15 @@
+/*
+
+Problem URL = https://leetcode.com/problems/monthly-transactions-i
+
+Select   
+    date_format(trans_date, "%Y-%m") as month, 
+    country,
+    count(state) as trans_count,
+    sum(if(state = 'approved', 1, 0)) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(if(state = 'approved', amount, 0)) as approved_total_amount
+from Transactions
+    Group By month, country
+
+*/
